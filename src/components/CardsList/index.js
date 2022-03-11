edc6a3cd6acc0b8fbdb50681c2025499c99b874b
Gabriel Lisboa da Silva <78692839+gabriellisboa08@users.noreply.github.com>
2022-03-11 16:29:31 -0300

    FinalApp
    aplicando dinamic backgroundImage na aplicação

--- src/components/CardsList/index.js
+++ src/components/CardsList/index.js
@@ -2,20 +2,17 @@ import "./CardsList.css";
 import Card from "../Card";
 import { useEffect, useState } from "react";
 import data from "../../api";
+import { margin } from "@mui/system";
 
 function CardsList() {
     const [characters, setCharacters] = useState([]);
     const [imagesBackGround, setImagesBackGround] = useState([
         "https://wallpaperaccess.com/full/6620899.jpg",
         "https://wallpaperaccess.com/full/1250862.jpg",
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/538a50bc-ccf9-4fd7-902c-69658ee473a8/dc0nr2u-a70f94f4-95f3-4ad9-9609-1c439d313548.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNTM4YTUwYmMtY2NmOS00ZmQ3LTkwMmMtNjk2NThlZTQ3M2E4XC9kYzBucjJ1LWE3MGY5NGY0LTk1ZjMtNGFkOS05NjA5LTFjNDM5ZDMxMzU0OC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.4g0xnTCJCFCegBiJtugkCn_cZ__4aMD8I3ymfQ6gCls",
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/38cd4d4a-deae-47bf-b8e7-4ab6d1cfee2a/d92er7a-15496c17-412d-4ad4-8998-34f4c1b4fa1b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM4Y2Q0ZDRhLWRlYWUtNDdiZi1iOGU3LTRhYjZkMWNmZWUyYVwvZDkyZXI3YS0xNTQ5NmMxNy00MTJkLTRhZDQtODk5OC0zNGY0YzFiNGZhMWIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WEv0cSGI_9YegbjfGDDOgEcF5AKSbkFA0KsGpQMhGTQ",
     ]);
-    const [indexImage, setIndexImage] = useState(0);
-
-    
-
-
-
-
+    const [scrollBackGround, setScrollBackGround] = useState(0);
 
     useEffect(async () => {
         const req = await data();
@@ -23,21 +20,30 @@ function CardsList() {
     }, []);
 
     useEffect(() => {
-        while (indexImage == 1) {
-            setTimeout(setIndexImage(indexImage + 1), 3000);
-        }
-    }, [indexImage]);
+        setTimeout(() => {
+            let x = scrollBackGround - 100;
+            if (x < imagesBackGround.length * -100 + 100) {
+                x = 0;
+            }
+            setScrollBackGround(x);
+        }, 10000);
+    }, [scrollBackGround]);
 
     return (
         <div className="container">
             <div className="container--backgroundImage">
-                <div className="backgroundImage--carousel--items">
-                    <div className="backgroundImage--item">
-                        <img src={imagesBackGround[indexImage]}></img>
-                    </div>
-                    <div className="backgroundImage--item">
-                        <img src={imagesBackGround[1]}></img>
-                    </div>
+                <div
+                    className="backgroundImage--carousel--items"
+                    style={{
+                        width: `${imagesBackGround.length * 100}vw`,
+                        marginLeft: `${scrollBackGround}vw`,
+                    }}
+                >
+                    {imagesBackGround.map((image, key) => (
+                        <div key={key} className="backgroundImage--item">
+                            <img src={image}></img>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="container--app">
